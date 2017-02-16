64e6444c8d5cc4c5c3df73bf80e56c940834cc13
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:41 +0100

    aureooms- > @aureooms/ : update js code

--- src/test.js
+++ src/test.js
@@ -1,10 +1,10 @@
 
-import sort from "aureooms-js-sort" ;
-import array from "aureooms-js-array" ;
-import random from "aureooms-js-random" ;
-import compare from "aureooms-js-compare" ;
-import * as itertools from "aureooms-js-itertools" ;
-import functools from "aureooms-js-functools" ;
+import sort from "@aureooms/js-sort" ;
+import array from "@aureooms/js-array" ;
+import random from "@aureooms/js-random" ;
+import compare from "@aureooms/js-compare" ;
+import * as itertools from "@aureooms/js-itertools" ;
+import functools from "@aureooms/js-functools" ;
 
 function check ( _test , sortname , method , ctor , n , comparename , compare ) {
 
